c3356c7f5a99209c9880b86c5ff47073760bd6c0
AaronEC <aaron_cardwell@hotmail.com>
2023-03-08 21:59:10 -0800

    Converstion logic added

--- converter/main/views.py
+++ converter/main/views.py
@@ -1,6 +1,10 @@
 import json
 import os
+import requests
+
 from django.shortcuts import render
+
+from .forms import InputForm
 
 def currency():
     """This imports the current currency list of data from a JSON
@@ -14,8 +18,46 @@ def currency():
 
     with open(location, "r") as f:
         currency = json.loads(f.read())
+        
     return currency
 
 def index(request):
+    """Main page view which has the coverter in it."""
+    
+    if request.method == "POST":
+
+        # User input data is requested from HTML.
+        input = request.POST.get('input')
+        output = request.POST.get('output')
+        form = InputForm(request.POST)
+        amount = float(form['input_field'].value())
+        
+        # API call for the current exchange rate data.
+        data = requests.get(f"https://open.er-api.com/v6/latest/{input}").json()
+        
+        # get amount to multiply input by.
+        multiple = data['rates'][output]
+        converted = format(float(multiple * amount),',.2f') # 2 decimal places
+        
+        context = {
+            'converted': converted,
+            'output': output,
+            'input': input,
+            'amount': amount,
+            'currency': currency(),
+            'form': InputForm(initial={'input_field': amount})
+        }
+        
+        return render(request, "index.html", context)
+        
+    form = InputForm(initial={'input_field': 50})
+        
+    
+    context = {
+        'currency': currency(),
+        'input': 'USD',     # These are the defaults
+        'output': 'EUR',     # Currently the worlds most traded currencies
+        'form': InputForm()
+    }
    
-    return render(request, "index.html", {"currency":currency()})
+    return render(request, "index.html", context)
