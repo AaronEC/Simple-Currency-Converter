43d4d719c0a2da3ca12deeca65d9e288ae56c824
AaronEC <aaron_cardwell@hotmail.com>
2023-03-08 20:20:22 -0800

    Basic index page view with inputs etc

--- converter/main/views.py
+++ converter/main/views.py
@@ -1,3 +1,21 @@
+import json
+import os
 from django.shortcuts import render
 
-# Create your views here.
+def currency():
+    """This imports the current currency list of data from a JSON
+
+    Returns:
+        dict: dict containing currency data.
+    """
+    
+    json_dir = os.path.dirname(__file__)
+    location = os.path.join(json_dir, 'currencies.json')
+
+    with open(location, "r") as f:
+        currency = json.loads(f.read())
+    return currency
+
+def index(request):
+   
+    return render(request, "index.html", {"currency":currency()})
